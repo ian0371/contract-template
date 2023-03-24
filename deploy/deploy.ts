4659220f5068ff09a22b00547665a6dbe1965947
Chihyun Song <ian0371@gmail.com>
2023-03-24 15:24:34 +0900

    Add hardhat deploy

---
+++ deploy/deploy.ts
@@ -0,0 +1,25 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("Lock", {
+    from: deployer,
+    gasLimit: 4000000,
+    args: [9999999999],
+    log: true,
+  });
+
+  await deploy("Counter", {
+    from: deployer,
+    gasLimit: 4000000,
+    args: [],
+    log: true,
+  });
+};
+
+export default func;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,6 +18,11 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545",
     },
   },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
 };
 
 export default config;
