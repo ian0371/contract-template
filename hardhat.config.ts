1bda45ffeec1bb9cf49b7643a3db60fc7bc0fe16
Chihyun Song <ian0371@gmail.com>
2023-03-31 13:49:48 +0900

    Add default key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,26 +4,30 @@ import "@nomicfoundation/hardhat-foundry";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 
+// the first key of test-junk
+const defaultKey =
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     baobab: {
       url: process.env.BAOBAB_URL || "https://archive-en.baobab.klaytn.net",
       chainId: 1001,
-      accounts: [process.env.PRIVATE_KEY as string],
+      accounts: [process.env.PRIVATE_KEY || defaultKey],
       live: true,
       saveDeployments: true,
     },
     cypress: {
       url: process.env.CYPRESS_URL || "https://archive-en.cypress.klaytn.net",
       chainId: 8217,
-      accounts: [process.env.PRIVATE_KEY as string],
+      accounts: [process.env.PRIVATE_KEY || defaultKey],
       live: true,
       saveDeployments: true,
     },
     local: {
       url: "http://127.0.0.1:8545",
-      accounts: [process.env.PRIVATE_KEY as string],
+      accounts: [process.env.PRIVATE_KEY || defaultKey],
       live: false,
       saveDeployments: true,
     },
